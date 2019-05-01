348d534bf2eb2a54533be61391bb130552e447c0
Nicolas Perez <pereznicolas90@gmail.com>
2019-04-30 22:06:24 -0500

    Trying out edit function

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ bot.on("message", async message => {
 
     if (cmd === `${prefix}bonzo`){
         //hello world command
-        return message.channel.send("hi, bonzo made me")
+
+        message.channel.send("hi, bonzo made me");
+
+        let chan = bot.channels.get("566030799573483524");
+        let lastMessage;
+
+        chan.fetchMessages({ limit: 1 }).then(messages => {
+            lastMessage = messages.first();
+            console.log("fetchin messages...")
+            if (!lastMessage.author.bot) {
+              console.log("last message's author is not a bot!")
+            }
+        })
+        .catch(console.error);
+        
+        setTimeout(function(){ 
+            lastMessage.edit('i say this now')
+            .then(msg => console.log(`New message content: ${msg}`))
+            .catch(console.error);
+        }, 3000);
+        return;
     }
 
     if (cmd === `${prefix}info`){
@@ -30,6 +50,22 @@ bot.on("message", async message => {
 
         return message.channel.send(botembed);
     }
+
+    if (cmd === `${prefix}dmg`){
+        let dmgstatus = new Discord.RichEmbed()
+        .setTitle("This is the title")
+        .setDescription("This is the description aaaaaaaaaaaaaaaaaaaaaaaah");
+
+        return message.channel.send(dmgstatus);
+    }
+
+    if (cmd === `${prefix}updatebonzo`){
+       // message.delete().catch(O_o=>{});
+        message.edit('i say this now')
+        .then(msg => console.log(`New message content: ${msg}`))
+        .catch(console.error);
+    }
+
 });
 
 bot.login(botconfig.token);
